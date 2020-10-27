929e2438ec295d17a277bb7dc87af569340a8524
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-10-27 13:17:39 +0100

    Refactor with Nivaes.TimeLine.WinUI

--- Nivaes.TimeLine.WinUI.UWP.Sample/App.xaml.cs
+++ Nivaes.TimeLine.WinUI.UWP.Sample/App.xaml.cs
@@ -1,26 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-namespace Nivaes.TimeLine.UAP.Sample
+﻿namespace Nivaes.TimeLine.WinUI.UWP.Sample
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.InteropServices.WindowsRuntime;
+    using Windows.ApplicationModel;
+    using Windows.ApplicationModel.Activation;
+    using Windows.Foundation;
+    using Windows.Foundation.Collections;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Controls.Primitives;
+    using Windows.UI.Xaml.Data;
+    using Windows.UI.Xaml.Input;
+    using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml.Navigation;
+
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    sealed partial class App
+        : Application
     {
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
---
+++ Nivaes.TimeLine.WinUI.WPF.Sample/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Nivaes.TimeLine.WinUI.WPF.Sample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+        : Application
+    {
+    }
+}
