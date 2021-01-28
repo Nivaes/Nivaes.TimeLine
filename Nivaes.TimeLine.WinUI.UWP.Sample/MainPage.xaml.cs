87a14363aa3835834a87c5f2b680ff0be04d73d6
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2021-01-28 21:58:57 +0100

    Include CI.

--- Nivaes.TimeLine.WinUI.UWP.Sample/MainPage.xaml.cs
+++ Nivaes.TimeLine.WinUI.UWP.Sample/MainPage.xaml.cs
@@ -1,19 +1,6 @@
 ﻿namespace Nivaes.TimeLine.WinUI.UWP.Sample
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Runtime.InteropServices.WindowsRuntime;
-    using Windows.Foundation;
-    using Windows.Foundation.Collections;
-    using Windows.UI.Xaml;
-    using Windows.UI.Xaml.Controls;
-    using Windows.UI.Xaml.Controls.Primitives;
-    using Windows.UI.Xaml.Data;
-    using Windows.UI.Xaml.Input;
-    using Windows.UI.Xaml.Media;
-    using Windows.UI.Xaml.Navigation;
+    using Microsoft.UI.Xaml.Controls;
 
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
